3a804e14dd6d1b018004a3e96ccd43ce730fd04d
blantality <parsoffgambit@gmail.com>
2025-10-25 12:45:09 +0300

    Sequence processing

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 int main() {
   using u_t = unsigned;
   u_t a = 0;
+  size_t count = 0;
   std::cin >> a;
+  while (std::cin >> a) {
+    count++;
+  }
   
   if (std::cin.eof()) {
-    std::cout << 0;
+    std::cout << count;
     std::cout << "\n";
   } else if (std::cin.fail()) {
     std::cerr << "Error\n";
